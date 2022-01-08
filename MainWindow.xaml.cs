4753ea45cb6ea51211797679aff7db023b3e7393
Sylvain125 <kenjii@hotmail.fr>
2022-01-08 19:04:07 +0100

    Ajoutez des fichiers projet.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Hellotext.Text = "Hello World !";
+        }
+
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(TextNameAdd.Text) && !ListBoxAdd.Items.Contains(TextNameAdd.Text))
+            {
+                ListBoxAdd.Items.Add(TextNameAdd.Text);
+                TextNameAdd.Clear();
+            }
+        }
+
+        private void Button_OpenFile(object sender, RoutedEventArgs e)
+        {
+            // Configure open file dialog box
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.FileName = ""; // Default file name
+            dialog.DefaultExt = ".txt"; // Default file extension
+            dialog.Filter = "Text documents (.txt)|*.txt"; // Filter files by extension
+
+            // Show open file dialog box
+            bool? result = dialog.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                string filename = dialog.FileName;
+                Result.Text = File.ReadAllText(dialog.FileName);
+                //Result.app
+            }
+        }
+
+        private void Button_SaveFile(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.DefaultExt = ".txt"; // Default file extension
+            saveFileDialog.Filter = "Text documents (.txt)|*.txt"; // Filter files by extension
+
+            if (saveFileDialog.ShowDialog() == true)
+                File.WriteAllText(saveFileDialog.FileName, Result.Text);
+        }
+    }
+}
